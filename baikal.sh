de56fe625aca265eb2a9d7256edb8e6c53fe5b68
minerstat <hello@coinscrow.io>
2018-02-26 13:03:55 +0100

    Update baikal.sh

--- baikal.sh
+++ baikal.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 exec 3<>/dev/tcp/127.0.0.1/4028
+echo "summary+pools+devs" 1>&3
+response="$(cat <&3)"
 
-MESSAGE="summary+pools+devs"
-
-echo -ne $MESSAGE >&3
-printf $MESSAGE >&3
+echo $response
